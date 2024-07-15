edc985c47ab9bb71469e4d85b0cc698ddd696546
Giuseppe Moschetta <giu.mos.dev@gmail.com>
2024-07-15 14:20:17 +0200

    first commit

---
+++ LinkedList.js
@@ -0,0 +1,33 @@
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  insert(data) {
+    const newNode = new Node(data);
+    if (!this.head) {
+      this.head = newNode;
+      return;
+    }
+    let current = this.head;
+    const values = [];
+    while (current) {
+      values.push(current.data);
+      current = current.next;
+    }
+    console.log(valus.join(" -> "));
+  }
+
+  reverse() {
+    let prev = null;
+    let current = this.head;
+    let next = null;
+    while (current) {
+      next = current.next;
+      current.next = prev;
+      prev = current;
+      current = next;
+    }
+    this.head = prev;
+  }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+This project involves implementing a singly linked list in JavaScript and adding functionality to reverse the list.
